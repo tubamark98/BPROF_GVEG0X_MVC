01d772e4b47907aa7addd9e87442f1e46c2b2530
Tuba Márk <val.afo00@gmail.com>
2020-11-24 06:30:37 +0100

    2nd repo here we go

---
+++ WebApplication/Repos/TrainerRepository.cs
@@ -0,0 +1,39 @@
+﻿using Data;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Repos
+{
+    public class TrainerRepository : IRepoBase<Models.Trainer>
+    {
+        readonly GymContext context = new GymContext();
+
+        public void Add(Trainer item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Trainer item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Trainer> Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(string gymID, Trainer newItem)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
