97522ab149c398ff4a95cc533ea760002f16fb1b
Tuba Márk <val.afo00@gmail.com>
2020-11-24 05:58:45 +0100

    everything broke idk

---
+++ WebApplication/Repos/ClientRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repos
+{
+    public class ClientRepository : IRepoBase<GymClient>
+    {
+        public void Add(GymClient item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(GymClient item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public System.Linq.IQueryable<GymClient> Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(string gymID, GymClient newItem)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
