01e3ce40ee8e9c8539143ae619f150d82fca31b4
Tuba Márk <val.afo00@gmail.com>
2020-11-24 06:28:19 +0100

    Added trainers

---
+++ WebApplication/Models/Trainer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Models
+{
+    public class Trainer
+    {
+        [Key]
+        public string TrainerID { get; set; }
+        [StringLength(200)]
+        public string TrainerName { get; set; }
+        public virtual ICollection<GymClient> GymClients { get; set; }
+        public Trainer()
+        {
+            GymClients = new List<GymClient>();
+        }
+    }
+}
