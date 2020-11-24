30ef272dafd6aca6a7e572cefb89bfdb335ffc34
Tuba Márk <val.afo00@gmail.com>
2020-11-24 05:49:17 +0100

    added repo & logic class lib

---
+++ WebApplication/Repos/IRepoBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Repos
+{
+    public interface IRepoBase<T> where T:new()
+    {
+        void Add(T item);
+        void Delete(T item);
+        IQueryable<T> Read();
+        void Update(string gymID, T newItem);
+        void Save();
+    }
+}
