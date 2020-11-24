2cb45953d461cfe23a64b8ffabb6ad8d98f42d10
Tuba Márk <val.afo00@gmail.com>
2020-11-24 05:27:34 +0100

    Model Added

---
+++ WebApplication/Models/GymClient.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Models
+{
+    public enum gender
+    {
+        Nő,
+        Férfi
+    }
+
+    public class GymClient
+    {
+        [Key]
+        public string GymID { get; set; }
+        [StringLength(200)]
+        public string FullName { get; set; }
+        [Range(14,100)]
+        public int Age { get; set; }
+        public gender Gender { get; set; }
+        [Range(0, 86)]
+        public int BeenWorkingOutFor { get; set; }
+        public bool Verified { get; set; }
+    }
+}
