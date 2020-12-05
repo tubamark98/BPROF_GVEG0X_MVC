7586ee591be563810d3fa12d69124eb02ddeb545
Tuba Márk <val.afo00@gmail.com>
2020-12-05 20:06:47 +0100

    createclient még nincs teljesen kész de mára végeztem

--- WebApplication/WebApplication/Controllers/HomeController.cs
+++ WebApplication/WebApplication/Controllers/HomeController.cs
@@ -3,14 +3,60 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Logic;
+using Models;
 
 namespace WebApplication.Controllers
 {
     public class HomeController : Controller
     {
+        TrainerLogic trainerLogic;
+        ClientLogic clientLogic;
+
+        public HomeController(TrainerLogic trainerLogic, ClientLogic clientLogic)
+        {
+            this.trainerLogic = trainerLogic;
+            this.clientLogic = clientLogic;
+        }
+        public IActionResult Init()
+        {
+            trainerLogic.FillDbWithSamples();
+            return RedirectToAction(nameof(Index));
+        }
         public IActionResult Index()
         {
+            return View(trainerLogic.GetTrainers());
+        }
+
+        public IActionResult GetTrainer(string trainerId)
+        {
+            return View(trainerLogic.GetTrainer(trainerId));
+        }
+
+        [HttpGet]
+        public IActionResult CreateTrainer()
+        {
             return View();
+        }
+        [HttpPost]
+        public IActionResult CreateTrainer( Trainer trainer)
+        {
+            trainer.TrainerID = Guid.NewGuid().ToString();
+            trainerLogic.AddTrainer(trainer);
+            return RedirectToAction(nameof(Index));
+        }
+        [HttpGet]
+        public IActionResult CreateClient()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CreateClient(string trainerId, GymClient gymClient)
+        {
+            gymClient.GymID = Guid.NewGuid().ToString(); 
+            gymClient.TrainerID = trainerId;
+            trainerLogic.AddClientToTrainer(gymClient, trainerId);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
--- WebApplication/WebApplication/Startup.cs
+++ WebApplication/WebApplication/Startup.cs
@@ -2,11 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Logic;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Models;
+using Repos;
 
 namespace WebApplication
 {
@@ -16,7 +19,11 @@ namespace WebApplication
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc((opt) => opt.EnableEndpointRouting = false);
+            services.AddMvc((opt)=> opt.EnableEndpointRouting =false);
+            services.AddTransient<TrainerLogic, TrainerLogic>();
+            services.AddTransient<ClientLogic, ClientLogic>();
+            services.AddTransient<IRepoBase<GymClient>, ClientRepository>();
+            services.AddTransient<IRepoBase<Trainer>, TrainerRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
