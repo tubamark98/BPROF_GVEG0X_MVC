87c18a23b8197e7c94ef3cfed771b5685e67f85b
Tuba Márk <val.afo00@gmail.com>
2020-12-15 06:02:30 +0100

    Made logictest class and installed moq+NUnit

--- WebApplication/Logic.Test/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Logic.Test
-{
-    public class Class1
-    {
-    }
-}
---
+++ WebApplication/Logic.Test/LogicTest.cs
@@ -0,0 +1,27 @@
+﻿using Moq;
+using NUnit.Framework;
+using Models;
+using Repos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logic.Test
+{
+    [TestFixture]
+    public class LogicTest
+    {
+        Mock<ClientRepository> clientRepository;
+        Mock<DetailRepository> detailRepository;
+        Mock<InfoRepository> infoRepository;
+        Mock<TrainerRepository> trainerRepository;
+
+        [SetUp]
+        public void Initialize()
+        {
+
+        }
+
+
+    }
+}
