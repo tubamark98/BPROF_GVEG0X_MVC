86888dd9b92e85bf25cf1b5fc077860859eacaf2
Tuba Márk <val.afo00@gmail.com>
2020-12-15 03:25:36 +0100

    In the middle of adding 2 more models

--- WebApplication/Models/WorkoutDetail.cs
+++ WebApplication/Models/WorkoutDetail.cs
@@ -1,15 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace Models
 {
+    public enum WorkoutTypes
+    {
+        calisthenics, crossfit, weightlifting, powerlifting
+    }
+    public enum ContestDiets
+    {
+        carbCycling, intermittentFasting, lowCarb, 
+    }
     public class WorkoutDetail
     {
         [Key]
         public string WorkoutId { get; set; }
-        public string Detail { get; set; }
-
+        public WorkoutTypes WorkoutType { get; set; }
+        public ContestDiets ContestDiets { get; set; }
+        [NotMapped]
+        public virtual GymClient GymClient { get; set; }
+        public virtual ICollection<ExtraInfo> AdditionalInfo { get; set; }
     }
 }
