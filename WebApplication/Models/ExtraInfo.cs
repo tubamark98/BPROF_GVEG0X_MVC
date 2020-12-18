d564fb7f4f3191711eeaee151abc9e8109b82342
Tuba Márk <val.afo00@gmail.com>
2020-12-18 17:53:13 +0100

    small fix

--- WebApplication/Models/ExtraInfo.cs
+++ WebApplication/Models/ExtraInfo.cs
@@ -10,6 +10,7 @@ namespace Models
     {
         [Key]
         public string InfoId { get; set; }
+        [StringLength(200)]
         public string Information { get; set; }
         public string GymID { get; set; }
 
