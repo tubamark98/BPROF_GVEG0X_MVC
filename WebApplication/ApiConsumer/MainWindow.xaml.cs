6f4b81e9459c252d87ab0f65480637ca3e543964
Tuba Márk <val.afo00@gmail.com>
2021-04-23 13:23:27 +0200

    in the middle of making the wpf app

--- WebApplication/ApiConsumer/MainWindow.xaml.cs
+++ WebApplication/ApiConsumer/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,36 @@ namespace ApiConsumer
         public MainWindow()
         {
             InitializeComponent();
+            GetTrainerNames();
         }
+
+        public async Task GetTrainerNames()
+        {
+            cbox.ItemsSource = null;
+            RestService restservice = new RestService("https://localhost:5001/", "/Trainer");
+            IEnumerable<Trainer> playlistnames =
+                await restservice.Get<Trainer>();
+
+            cbox.ItemsSource = playlistnames;
+            cbox.SelectedIndex = 0;
+        }
+
+        /*
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            GymClient newvideo = new GymClient()
+            {
+                FullName = tb_name.Text,
+                Title = tb_title.Text,
+                YoutubeId = tb_youtube.Text,
+                Rating = 5,
+                PlayListUid = (cbox.SelectedItem as Playlist).UID
+            };
+
+            RestService restservice = new RestService("https://localhost:5001/", "/Video");
+            restservice.Post<GymClient>(newvideo);
+            GetTrainerNames();
+        }
+        */
     }
 }
